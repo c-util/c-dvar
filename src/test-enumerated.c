e5ef938c2fa73fe82dfe4a0e9ac0d48d4c201abc
David Herrmann <dh.herrmann@gmail.com>
2017-03-31 15:48:23 +0200

    test: add DBusTypenum based systematic type-testing
    Use the DBusTypenum type generator to do systematic type-testing for
    c_dvar_type_new_from_signature(). This allows us to verify all possible
    D-Bus types mechanically.
    
    Note that there is currently no way to do the inverse operation, since
    we do not have an inverse for DBUsTypenum right now.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

---
+++ src/test-enumerated.c
@@ -0,0 +1,75 @@
+/*
+ * Test / Data Verification with Enumerated Types
+ *
+ * Use the dbus-typenum library to enumerate D-Bus types systematically. Then
+ * verify we can deal with them correctly. All of them.
+ */
+
+#include <assert.h>
+#include <dbus-typenum.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "c-dvar.h"
+#include "c-dvar-private.h"
+
+static void test_valid_type(const char *typestr, size_t n_typestr) {
+        CDVarType *type;
+        int r;
+
+        /* verify @typestr is a valid type */
+
+        r = c_dvar_type_new_from_signature(&type, typestr, n_typestr);
+        assert(!r);
+        assert(type->length == n_typestr);
+        c_dvar_type_free(type);
+}
+
+static void test_enumerated_types(void) {
+        DBusTypenum *e;
+        char *str = NULL, *pos = NULL;
+        size_t i, n_str = 0;
+        char c;
+        int r;
+
+        /*
+         * Enumerate valid types via DBusTypenum and check that all generated
+         * types are accepted by c-dvar.
+         */
+
+        r = dbus_typenum_new(&e, 0);
+        assert(!r);
+
+        n_str = 2;
+        str = malloc(n_str);
+        assert(str);
+        pos = str;
+
+        for (i = 0; i < 1 << 16; ++i) {
+                dbus_typenum_seed_u32(e, i);
+
+                do {
+                        c = dbus_typenum_step(e);
+                        if (pos - str >= n_str) {
+                                str = realloc(str, (pos - str) * 2);
+                                assert(str);
+                                pos = str + n_str;
+                                n_str *= 2;
+                        }
+
+                        *pos++ = c;
+                } while (c);
+
+                test_valid_type(str, pos - str - 1);
+                pos = str;
+                dbus_typenum_reset(e);
+        }
+
+        free(str);
+        dbus_typenum_free(e);
+}
+
+int main(int argc, char **argv) {
+        test_enumerated_types();
+        return 0;
+}
